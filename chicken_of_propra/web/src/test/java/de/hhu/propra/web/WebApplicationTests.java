e0a2eaeac1619644bdc96c82cf8758fcafb0db2d
Euchi <yassineeuchi2254@gmail.com>
2022-03-07 12:29:33 +0100

    Gradle Subprojekte Onion Architektur

---
+++ chicken_of_propra/web/src/test/java/de/hhu/propra/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.propra.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
