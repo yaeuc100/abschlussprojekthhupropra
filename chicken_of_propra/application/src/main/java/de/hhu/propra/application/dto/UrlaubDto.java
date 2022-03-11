7c3b4fbcb52f5088aeadb5d8d929c4078719438a
Euchi <yassineeuchi2254@gmail.com>
2022-03-11 13:31:48 +0100

    (UrlaubKlausurValidierungTests) Test zu freieZeitDurchKlausur geschrieben

--- chicken_of_propra/application/src/main/java/de/hhu/propra/application/dto/UrlaubDto.java
+++ chicken_of_propra/application/src/main/java/de/hhu/propra/application/dto/UrlaubDto.java
@@ -2,6 +2,19 @@ package de.hhu.propra.application.dto;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
 public record UrlaubDto(LocalDate datum, LocalTime startzeit, LocalTime endzeit) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UrlaubDto urlaubDto = (UrlaubDto) o;
+        return datum.equals(urlaubDto.datum) && startzeit.equals(urlaubDto.startzeit) && endzeit.equals(urlaubDto.endzeit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(datum, startzeit, endzeit);
+    }
 }
--- chicken_of_propra/application/src/test/java/de/hhu/propra/application/utils/UrlaubKlausurValidierungTests.java
+++ chicken_of_propra/application/src/test/java/de/hhu/propra/application/utils/UrlaubKlausurValidierungTests.java
@@ -1,4 +1,62 @@
 package de.hhu.propra.application.utils;
 
+import de.hhu.propra.application.dto.UrlaubDto;
+import de.hhu.propra.domain.aggregates.klausur.Klausur;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UrlaubKlausurValidierungTests {
+
+    UrlaubKlausurValidierung urlaubKlausurValidierung = new UrlaubKlausurValidierung();
+
+
+
+    @Test
+    @DisplayName("Zeit der Klausur wird zu Urlaub(Online)")
+    void test1(){
+        //arrange
+        Klausur klausur = new Klausur(null,
+                null,
+                 LocalDateTime.of(3000,3,5,10,00),
+                60,
+                1L,
+                true);
+        UrlaubDto urlaubDto = new UrlaubDto(klausur.datum().toLocalDate(),
+                LocalTime.of(9,30),
+                LocalTime.of(11,00));
+
+        //act
+        UrlaubDto ergebnis = urlaubKlausurValidierung.freieZeitDurchKlausur(klausur);
+
+        //assert
+        assertThat(ergebnis).isEqualTo(urlaubDto);
+
+    }
+    @Test
+    @DisplayName("Zeit der Klausur wird zu Urlaub(Offline)")
+    void test2(){
+        //arrange
+        Klausur klausur = new Klausur(null,
+                null,
+                LocalDateTime.of(3000,3,5,10,00),
+                60,
+                1L,
+                false);
+        UrlaubDto urlaubDto = new UrlaubDto(klausur.datum().toLocalDate(),
+                LocalTime.of(8,00),
+                LocalTime.of(13,00));
+
+        //act
+        UrlaubDto ergebnis = urlaubKlausurValidierung.freieZeitDurchKlausur(klausur);
+
+        //assert
+        assertThat(ergebnis).isEqualTo(urlaubDto);
+
+    }
+
 }
