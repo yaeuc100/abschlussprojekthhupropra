5484107d59ea97c8f60f4e19a32f784d84bb0de7
iTitus <iTitus@users.noreply.github.com>
2022-03-29 15:13:56 +0200

    fix date & time injection

--- chicken_of_propra/application/src/main/java/de/hhu/propra/application/utils/Praktikumskonfiguration.java
+++ chicken_of_propra/application/src/main/java/de/hhu/propra/application/utils/Praktikumskonfiguration.java
@@ -6,35 +6,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class Praktikumskonfiguration{
 
   LocalDate datumStart;
   LocalDate datumEnde;
   LocalTime zeitStart;
   LocalTime zeitEnde;
-/*
-  public Praktikumskonfiguration(@Value("#{T(java.time.LocalDate).parse('${praktikum.datum.start}')}") LocalDate datumStart,
-      @Value("#{T(java.time.LocalDate).parse('${praktikum.datum.ende}')}") LocalDate datumEnde,
-      @Value("#{T(java.time.LocalTime).parse('${praktikum.zeit.start}')}") LocalTime zeitStart,
-      @Value("#{T(java.time.LocalTime).parse('${praktikum.zeit.ende}')}") LocalTime zeitEnde){
+
+  public Praktikumskonfiguration(
+          @Value("${praktikum.datum.start}") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate datumStart,
+          @Value("${praktikum.datum.ende}") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate datumEnde,
+          @Value("${praktikum.zeit.start}") @DateTimeFormat(pattern = "HH:mm") LocalTime zeitStart,
+          @Value("${praktikum.zeit.ende}") @DateTimeFormat(pattern = "HH:mm") LocalTime zeitEnde) {
     this.datumStart=datumStart;
     this.datumEnde=datumEnde;
     this.zeitStart=zeitStart;
     this.zeitEnde=zeitEnde;
   }
-*/
-/*
-@Bean
-public Praktikumskonfiguration konfiguration(@Value("#{T(java.time.LocalDate).parse('${praktikum.datum.start}')}") LocalDate datumStart,
-    @Value("#{T(java.time.LocalDate).parse('${praktikum.datum.ende}')}") LocalDate datumEnde,
-    @Value("#{T(java.time.LocalTime).parse('${praktikum.zeit.start}')}") LocalTime zeitStart,
-    @Value("#{T(java.time.LocalTime).parse('${praktikum.zeit.ende}')}") LocalTime zeitEnde){
-  return new Praktikumskonfiguration();
-}
-*/
-
 
   public LocalDate getDatumStart() {
     return datumStart;
